cc2cd477018d8552b6c995a927dc78763fd4f022
Thiago <thiagokriegercardoso@gmail.com>
2021-09-10 17:50:31 -0300

    Adicionar arquivos de projeto.

---
+++ APILocal/APIService.cs
@@ -0,0 +1,17 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APILocal
+{
+     public interface APIService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+
+
+
+    }
+}
---
+++ APILocal/Program.cs
@@ -0,0 +1,31 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace APILocal
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepCliente = RestService.For<APIService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu CEP: ");
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando Informações do CEP " + cepInformado);
+
+
+                var address = await cepCliente.GetAddressAsync(cepInformado);
+
+                Console.WriteLine($"\nLogradouro:{address.logradouro},\nBairro:{address.bairro},\nLocalidade:{address.localidade},\nDDD:{address.ddd}");
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Erro na Consulta de CEP." + e.Message);
+            }
+        }
+    }
+}
